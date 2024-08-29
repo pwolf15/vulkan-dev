63d51c043e460cb3c1ae457aaf8d6c214dfc8f29
pwolf15 <patrick.wolf@live.com>
2024-08-29 13:32:29 +0200

    Finished first part of triangle tutorial
    * TODO list in README

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Install Vulkan: https://vulkan.lunarg.com/doc/view/1.3.268.1/mac/getting_started
 ```bash
 source ~/VulkanSDK/1.3.290.0/setup-env.sh
 ```
+
+## TODO Vulkan
+
+[ ] Vulkan Tutorial
+[ ] VK Guide
--- src/hello_triangle.cpp
+++ src/hello_triangle.cpp
@@ -10,55 +10,79 @@
 #include <stdexcept>
 #include <cstdlib>
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication 
 {
 public:
     void run()
     {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    void initWindow()
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
     void initVulkan()
     {
+        createInstance();
+    }
 
+    void createInstance()
+    {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
     }
 
     void mainLoop()
     {
-
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
     }
 
     void cleanup()
     {
-
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
+
+    GLFWwindow* window;
+    VkInstance instance;
 };
 
 int main() {
-    glfwInit();
+    HelloTriangleApplication app;
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    try 
+    {
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-
-    return 0;
+    return EXIT_SUCCESS;
 }
